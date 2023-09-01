799de0d4ec81d2856fd2ffc88214f114b26a0d34
ycoya <yu.coya@gmail.com>
2023-08-31 23:15:45 -0400

    allowing normal download if not header ranges received or better said no partial content receive, no 206. Any other status, save completly the file.

--- src/ClientResume.php
+++ src/ClientResume.php
@@ -1,14 +1,13 @@
 <?php
+
 namespace Ycoya\GuzzlehttpResume;
 
 require 'vendor/autoload.php';
 
-use DateTime;
 use GuzzleHttp\Client;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Middleware;
 use GuzzleHttp\Psr7\Header;
-use GuzzleHttp\RetryMiddleware;
 use GuzzleHttp\TransferStats;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
@@ -23,69 +22,92 @@ class ClientResume implements ClientResumeInterface
 
     protected ResponseInterface $response;
 
-    public int $downloadSize = 100*1024*1024;
+    public int $downloadSize = 100 * 1024 * 1024;
 
     public string $rangeUnit = "bytes";
 
 
     public function __construct(protected ?Client $client = null)
     {
-        if(!$this->client) {
+        if (!$this->client) {
             $this->client = new Client();
         }
         $this->stack = HandlerStack::create();
-        $this->stack->push(Middleware::retry($this->reSend(), $this->delayBetweenRequest()));
+        $this->stack->push(Middleware::retry($this->reTry(), $this->delayBetweenRequest()));
         $this->stack->push(Middleware::mapRequest($this->requestMiddleware()));
     }
 
-    private function delayBetweenRequest() : callable
+    public function setClient(Client $client)
     {
-        return function($retries) {
-            return 0.5 * 1000;
-        };
+        $this->client = $client;
     }
 
-    public function setFilename(string $filename)
+    public function getClient(): Client
+    {
+        return $this->client;
+    }
+
+    public function setHandler(HandlerStack $handlerStack)
+    {
+        $this->stack = $handlerStack;
+    }
+
+    public function getHandler(): HandlerStack
+    {
+        return $this->stack;
+    }
+
+    public function setFilename(string $filename): void
     {
         $this->filename = $filename;
+        $this->makePath($this->filename);
     }
 
-    public function resume($method, $url, $options)
+    public function resume($method, $url, $options): void
     {
         $options['handler'] = $this->stack;
-         if(Log::$debug) {
-            $options['on_stats'] = function(TransferStats $stats) {
+        if (Log::$debug) {
+            $options['on_stats'] = $options['on_stats'] ?? function (TransferStats $stats) {
                 $this->dumpStats($stats);
             };
         }
         $this->client->request($method, $url, $options);
     }
 
+    private function delayBetweenRequest(): callable
+    {
+        return function ($retries) {
+            return 500;
+        };
+    }
+
     protected function requestMiddleware()
     {
-        return function(RequestInterface $request){
+        return function (RequestInterface $request) {
             Log::debug("log", "starting request middleware");
             if (empty($this->filename)) {
                 $this->createFilenameFromRequest($request);
-                Log::debug("log", "file wasn't set, creating filename from request: $this->filename" );
+                $this->makePath($this->filename);
+                Log::debug("log", "file wasn't set, creating filename from request: $this->filename");
             }
             return $request->withHeader("Range", $this->getRangeForRequest());
         };
     }
 
-    protected function reSend()
+    protected function reTry()
     {
-        return function ($retries,
-                        RequestInterface $request,
-                        ResponseInterface $response = null,
-                        \Exception $exception = null
-        ){
+        return function (
+            $retries,
+            RequestInterface $request,
+            ResponseInterface $response = null,
+            \Exception $exception = null
+        ) {
             Log::debug("log", "starting retry middleware");
 
-            if(!is_null($response) && $response->getStatusCode() == 206 ) {
+            if (!is_null($response) && $response->getStatusCode() == 206) {
 
                 list($end, $filesize) = $this->parseResponseHeaderRange($response);
-                if($end +1 == $filesize) {
+                if ($end + 1 == $filesize) {
                     Log::debug("log", " download finished, retries: $retries" . PHP_EOL);
                     $this->savingFileFromResponse($response);
                     rename("$this->filename.part", $this->filename);
@@ -96,14 +118,11 @@ class ClientResume implements ClientResumeInterface
                 Log::debug("log", " saving partial data" . PHP_EOL);
                 return true;
             }
+            $this->savingFileFromResponse($response, false);
+            Log::debug("log", " retries: $retries, saving file, location:end of method for RetryMiddleware " . $response->getStatusCode() . PHP_EOL);
+            return false;
 
-            Log::debug("log"," retries: $retries, location:end of method for RetryMiddleware" . PHP_EOL );
 
-            if($retries >=5) {
-                return false;
-            }
-
-            return true;
         };
     }
 
@@ -119,7 +138,7 @@ class ClientResume implements ClientResumeInterface
 
     private function getRangeForRequest()
     {
-        if(is_file("$this->filename.part")) {
+        if (is_file("$this->filename.part")) {
             $rangeStart = filesize("$this->filename.part") + 1;
             $rangeEnd   = $rangeStart + $this->downloadSize;
             $range = "$this->rangeUnit=$rangeStart-$rangeEnd";
@@ -137,23 +156,23 @@ class ClientResume implements ClientResumeInterface
         $filename = $request->getUri()->getQuery();
         $filename = Header::parse($filename)[0] ?? [$request->getUri()->getPath()];
         $filename = $filename[array_key_first($filename)];
-        if(!empty($filename)) {
-            return $this->filename = $filename;
+        if (!empty($filename)) {
+            return $this->filename = "downloads/$filename";
         }
-        return $this->filename = $request->getUri()->getHost();
+        return $this->filename = "downloads/" . $request->getUri()->getHost();
     }
 
-    private function savingFileFromResponse(ResponseInterface $response): void
+    private function savingFileFromResponse(ResponseInterface $response, bool $partial = true): void
     {
-        file_put_contents("$this->filename.part", $response->getBody(), FILE_APPEND);
+        file_put_contents($partial ? "$this->filename.part" : $this->filename, $response->getBody(), FILE_APPEND);
     }
 
     private function dumpStats(TransferStats $stats)
     {
         $request = $stats->getRequest();
         $response = $stats->getResponse();
-        $data = "-- url: ".  $request->getUri() . " headers: " . json_encode($request->getHeaders()) . "----- responseHeaders: " . json_encode($response->getHeaders());
-        if(Log::$debugVerbose) {
+        $data = "-- url: " .  $request->getUri() . " headers: " . json_encode($request->getHeaders()) . "----- responseHeaders: " . json_encode($response->getHeaders());
+        if (Log::$debugVerbose) {
             $data .= " body: " . $response->getBody() . PHP_EOL;
         } else {
             $data .= PHP_EOL;
@@ -162,21 +181,21 @@ class ClientResume implements ClientResumeInterface
         Log::debug($statsPath, $data);
     }
 
-     // gotten from internet.
-     private function makePath($path)
-     {
-         $dir = pathinfo($path, PATHINFO_DIRNAME);
+    // gotten from internet.
+    private function makePath($path)
+    {
+        $dir = pathinfo($path, PATHINFO_DIRNAME);
 
-         if (is_dir($dir)) {
-             return true;
-         } else {
-             if ($this->makePath($dir)) {
-                 if (mkdir($dir)) {
-                     chmod($dir, 0777);
-                     return true;
-                 }
-             }
-         }
-         return false;
-     }
+        if (is_dir($dir)) {
+            return true;
+        } else {
+            if ($this->makePath($dir)) {
+                if (mkdir($dir)) {
+                    chmod($dir, 0777);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
--- src/Interfaces/ClientResumeInterface.php
+++ src/Interfaces/ClientResumeInterface.php
@@ -1,7 +1,23 @@
 <?php
-
+//phpcs:disable comment
 namespace Ycoya\GuzzlehttpResume\Interfaces;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+
+//phpcs:disable comment
 interface ClientResumeInterface
 {
+    public function resume($method, $url, $options): void;
+
+    public function setFilename(string $filename): void;
+
+    public function setClient(Client $client);
+
+    public function getClient(): Client;
+
+    public function setHandler(HandlerStack $handlerStack);
+
+    public function getHandler(): HandlerStack;
+
 }
